1ea38b0f06b30569bc3630d5f5fc2e33ef75df03
Janhavi Mallya <janhavimallya@gmail.com>
2026-05-31 12:18:36 +0530

    Fix formatting and syntax issues in Fact.java

---
+++ Fact.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+class Fact
+{
+  static int fact (int n)
+  {
+    int f=1;
+    for (int i=1;i<=n;i++)
+    {
+      f=f*i
+    }
+    return f;
+  }
+public static void main(String args[])
+{
+  Scanner S= new Scanner(System.im);
+  System.out.println("enter the number to be computed");
+  int n=S.nextInt();
+  System.out.println("fact="+fact(n));
+}
+}
